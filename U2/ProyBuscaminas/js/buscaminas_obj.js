2b651ac011f521f0b2831ec265a6274366a7c3c7
carlos_blanco <carlosblancofranco@gmail.com>
2022-10-18 18:26:09 +0200

    formateado

--- U2/ProyBuscaminas/js/buscaminas_obj.js
+++ U2/ProyBuscaminas/js/buscaminas_obj.js
@@ -1,5 +1,5 @@
 class Tablero {
-    constructor(filas, columnas){
+    constructor(filas, columnas) {
         this.filas = filas;
         this.columnas = columnas;
 
@@ -7,15 +7,15 @@ class Tablero {
         this.pintarTablero();
     }
 
-// Lo siguiente iria fuera de la clase (donde cierra la llave de la clase)
-// const tableroBuscaminas = new Tablero(3,5);
-// console.log(tableroBuscaminas);
-// Ctrl + a y luego Ctrl + alt + n para ver en la terminal como el constructor cre un objeto Tablero
+    // Lo siguiente iria fuera de la clase (donde cierra la llave de la clase)
+    // const tableroBuscaminas = new Tablero(3,5);
+    // console.log(tableroBuscaminas);
+    // Ctrl + a y luego Ctrl + alt + n para ver en la terminal como el constructor cre un objeto Tablero
 
-    crearTablero(){
+    crearTablero() {
         // Crear array bidimensional para guardar las minas
         this.arrayTablero = [];
-    
+
         for (let fila = 0; fila < this.filas; fila++) {
             this.arrayTablero[fila] = [];
 
@@ -25,36 +25,36 @@ class Tablero {
         }
     }
 
-    pintarTablero(){
+    pintarTablero() {
         // Creamos el tablero en html
         document.write('<table>');
-    
+
         for (let i = 0; i < this.filas; i++) {
             document.write('<tr>');
-        
+
             for (let j = 0; j < this.columnas; j++) {
-                if (this.crearTablero[i][j] != 0){
-                   document.write('<td>'+ this.crearTablero[i][j] +'</td>');
-                } else{
-                   document.write('<td></td>');
-               }
-           }
-       
-           document.write('</tr>');
-       }
-       document.write('</table>');
-       
-       }
+                if (this.crearTablero[i][j] != 0) {
+                    document.write('<td>' + this.crearTablero[i][j] + '</td>');
+                } else {
+                    document.write('<td></td>');
+                }
+            }
 
-
-
-
+            document.write('</tr>');
+        }
+        document.write('</table>');
 
     }
 
 
 
-const buscamminas = new Tablero(3,6);
+
+
+}
+
+
+
+const buscamminas = new Tablero(3, 6);
 console.log(buscamminas.filas);
 console.log(buscamminas.columnas);
 console.log(buscamminas.arrayTablero);
