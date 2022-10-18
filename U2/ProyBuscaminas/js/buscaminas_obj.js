2cabe97610b567dbd569bfb3b17ff1ca9eb45ca6
carlos_blanco <carlosblancofranco@gmail.com>
2022-10-18 19:09:42 +0200

    18/9 clase

--- U2/ProyBuscaminas/js/buscaminas_obj.js
+++ U2/ProyBuscaminas/js/buscaminas_obj.js
@@ -36,11 +36,13 @@ class Tablero {
     }
 
     modificarFilas(nuevasFilas){
+        // Modificar el número de filas y crear de nuevo el tablero
         this.filas = nuevasFilas;
         crearTablero();
     }
 
     modificarColumnas(nuevasColumnas){
+        // Modificar el número de columnas y crear de nuevo el tablero
         this.columnas = nuevasColumnas;
         crearTablero();
     }
@@ -49,6 +51,66 @@ class Tablero {
 
 
 }
-
+/*
 const buscaminas = new Tablero(4, 3);
 buscaminas.dibujarTablero();
+*/
+
+class Buscaminas extends Tablero{
+    constructor(filas, columnas, numMinas) {
+        super(filas, columnas);
+        this.numMinas = numMinas;
+
+        this.colocarMinas();
+        this.colocarNumMinas();
+    }
+
+    colocarMinas(){
+        let contadorMinas = 0;
+        let posFila;
+        let posColumna;
+    
+    
+        while (contadorMinas < this.numMinas) {
+            posFila = Math.floor(Math.random() * this.filas);
+            posColumna = Math.floor(Math.random() * this.columnas);
+    
+            if (this.arrayTablero[posFila][posColumna] != 'MINA') {
+                this.arrayTablero[posFila][posColumna] = 'MINA';
+                contadorMinas++;
+            };
+        };
+    }
+
+    colocarNumMinas(){
+        let numMinasAlrededor;
+    
+        for (let fila = 0; fila < this.filas; fila++) {
+            for (let columna = 0; columna < this.columnas; columna++) {
+                numMinasAlrededor = 0;
+                if (this.arrayTablero[fila][columna] != 'MINA') {
+                    for (let cFila = fila - 1; cFila <= fila + 1; cFila++) {
+                        if (cFila >= 0 && cFila < this.filas) {
+                            for (let cColumna = columna - 1; cColumna <= columna + 1; cColumna++) {
+                                if (cColumna >= 0 && cColumna < this.columnas &&
+                                    this.arrayTablero[cFila][cColumna] == 'MINA') {
+                                    numMinasAlrededor++;
+                                }
+                            }
+                        }
+                        this.arrayTablero[fila][columna] = numMinasAlrededor;
+                    }
+        
+                }
+            }
+        }
+    }
+
+
+
+
+}
+
+let buscaminas1 = new Buscaminas(5,5,5);
+console.log(buscaminas1.arrayTablero);
+buscaminas1.dibujarTablero();
