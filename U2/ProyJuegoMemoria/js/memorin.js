1afd3e37832c4b8a558dc6940fade51db183921b
carlos_blanco <carlosblancofranco@gmail.com>
2022-10-21 15:45:16 +0200

    Crear tablero

--- U2/ProyJuegoMemoria/js/memorin.js
+++ U2/ProyJuegoMemoria/js/memorin.js
@@ -8,3 +8,18 @@ while((maxFilas * maxColumnas) % 2 != 0){
     maxFilas = prompt('¿Cuantas filas quiere que tenga el tablero?');
     maxColumnas = prompt('¿Cuantas columnas quiere que tenga el tablero?');
 }
+
+// Crear tablero
+let arrayTablero = [];
+
+for (let fila = 0; fila < maxFilas; fila++) {
+    
+    arrayTablero[fila] = new Array(maxColumnas);
+
+    for (let columa = 0; columa < maxColumnas; columa++) {
+        
+        arrayTablero[fila][columa] = '';
+        
+    }
+    
+}
