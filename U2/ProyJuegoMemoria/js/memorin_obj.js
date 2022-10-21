2051919035591e469f472b92788dca5cc6ff52e5
carlos_blanco <carlosblancofranco@gmail.com>
2022-10-21 23:38:00 +0200

    separado todo en funciones

--- U2/ProyJuegoMemoria/js/memorin_obj.js
+++ U2/ProyJuegoMemoria/js/memorin_obj.js
@@ -8,89 +8,108 @@ while((maxFilas * maxColumnas) % 2 != 0){
     maxColumnas = prompt('¿Cuantas columnas quiere que tenga el tablero?');
 }
 
-let numCasillas = maxFilas * maxColumnas;
+tableroMemoria = crearArrayTablero(maxFilas, maxColumnas);
+tableroMemoria = colocarParejas(tableroMemoria, maxFilas, maxColumnas);
+pintarTablero(tableroMemoria, maxFilas, maxColumnas);
 
-// Crear tablero
-let arrayTablero = [];
+function crearArrayTablero(maxFilas, maxColumnas){
 
-for (let fila = 0; fila < maxFilas; fila++) {
+    // Crear tablero
+    let arrayTablero = [];
+
+    for (let fila = 0; fila < maxFilas; fila++) {
     
-    arrayTablero[fila] = new Array(maxColumnas);
+        arrayTablero[fila] = new Array(maxColumnas);
 
-    for (let columa = 0; columa < maxColumnas; columa++) {
+        for (let columa = 0; columa < maxColumnas; columa++) {
         
-        arrayTablero[fila][columa] = '';
+            arrayTablero[fila][columa] = '';
         
+        }
+    
     }
-    
+
+    return arrayTablero;
+
 }
 
-// Colocar parejas de forma equilibrada (que no haya, por ejemplo, 4 parejas de 2 y una del resto).
-let parejas = [
-                "/stickers/bluetooth.svg",
-                "/stickers/discord.svg",
-                "/stickers/gamepad-solid.svg",
-                "/stickers/java.svg",
-                "/stickers/jedi-order.svg",
-                "/stickers/php.svg",
-                "/stickers/square-github.svg", 
-                "/stickers/twitch.svg", 
-                "/stickers/user-ninja-solid.svg",
-                "/stickers/xbox.svg"
-            ];
-let posFila = 0;
-let posColumna = 0;
-let numParejas = 0;
-let contadorArray = 0;
+function colocarParejas(arrayTablero, maxFilas, maxColumnas){
 
-while (numParejas != (numCasillas/2)) {
+    // Colocar parejas de forma equilibrada (que no haya, por ejemplo, 4 parejas de 2 y una del resto).
+    let parejas = [
+        "/stickers/bluetooth.svg",
+        "/stickers/discord.svg",
+        "/stickers/gamepad-solid.svg",
+        "/stickers/java.svg",
+        "/stickers/jedi-order.svg",
+        "/stickers/php.svg",
+        "/stickers/square-github.svg", 
+        "/stickers/twitch.svg", 
+        "/stickers/user-ninja-solid.svg",
+        "/stickers/xbox.svg"
+    ];
+    let numCasillas = maxFilas * maxColumnas;
+    let posFila = 0;
+    let posColumna = 0;
+    let numParejas = 0;
+    let contadorArray = 0;
 
-    posFila = Math.floor(Math.random() * maxFilas);
-    posColumna = Math.floor(Math.random() * maxColumnas);
+    while (numParejas != (numCasillas/2)) {
 
-    while (arrayTablero[posFila][posColumna] != '') {
-        
         posFila = Math.floor(Math.random() * maxFilas);
         posColumna = Math.floor(Math.random() * maxColumnas);
 
-    }
+        while (arrayTablero[posFila][posColumna] != '') {
 
-    arrayTablero[posFila][posColumna] = parejas[contadorArray];
+            posFila = Math.floor(Math.random() * maxFilas);
+            posColumna = Math.floor(Math.random() * maxColumnas);
 
-    posFila = Math.floor(Math.random() * maxFilas);
-    posColumna = Math.floor(Math.random() * maxColumnas);
+        }
 
-    while (arrayTablero[posFila][posColumna] != '') {
-        
+        arrayTablero[posFila][posColumna] = parejas[contadorArray];
+
         posFila = Math.floor(Math.random() * maxFilas);
         posColumna = Math.floor(Math.random() * maxColumnas);
 
-    }
+        while (arrayTablero[posFila][posColumna] != '') {
 
-    arrayTablero[posFila][posColumna] = parejas[contadorArray];
+            posFila = Math.floor(Math.random() * maxFilas);
+            posColumna = Math.floor(Math.random() * maxColumnas);
 
-    contadorArray++;
-    numParejas++;
+        }
 
-    if (contadorArray == 10) {
+        arrayTablero[posFila][posColumna] = parejas[contadorArray];
+
+        contadorArray++;
+        numParejas++;
+
+        if (contadorArray == 10) {
         contadorArray = 0;
+        }
+
     }
-    
+
+    return arrayTablero;
+
 }
 
-// Pintar tablero en html
-document.write('<table>');
-
-for (let i = 0; i < maxFilas; i++) {
+function pintarTablero(arrayTablero, maxFilas, maxColumnas){
     
-    document.write('<tr>');
+    // Pintar tablero en html
+    document.write('<table>');
 
-    for (let j = 0; j < maxColumnas; j++) {
+    for (let i = 0; i < maxFilas; i++) {
+    
+        document.write('<tr>');
+
+        for (let j = 0; j < maxColumnas; j++) {
         
-        document.write('<td><img src=' + arrayTablero[i][j] + '></td>');
+            document.write('<td><img src=' + arrayTablero[i][j] + '></td>');
         
+        }
+        document.write('</tr>');
     }
-    document.write('</tr>');
-}
 
-document.write('</table>');
+    document.write('</table>');
+
+}
