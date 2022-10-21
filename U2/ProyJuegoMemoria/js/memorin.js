2aa20cab48bc58dde04cb3ced8bf5a2659c9a36c
carlos_blanco <carlosblancofranco@gmail.com>
2022-10-21 15:38:04 +0200

    solicitar tamaño pudiendo formar parejas

--- U2/ProyJuegoMemoria/js/memorin.js
+++ U2/ProyJuegoMemoria/js/memorin.js
@@ -1,1 +1,10 @@
 // Pedir datos al usuario
+let maxFilas = prompt('¿Cuantas filas quiere que tenga el tablero?');
+let maxColumnas = prompt('¿Cuantas columnas quiere que tenga el tablero?');
+
+
+while((maxFilas * maxColumnas) % 2 != 0){
+    alert('ERROR\nEl número de casillas debe permitir parejas');
+    maxFilas = prompt('¿Cuantas filas quiere que tenga el tablero?');
+    maxColumnas = prompt('¿Cuantas columnas quiere que tenga el tablero?');
+}
