b5e1daa3e0832d0fe9cfc245e44e5f627fba850d
carlos_blanco <carlosblancofranco@gmail.com>
2023-01-24 19:09:25 +0100

    24 enero clase

--- U5/projecte5_Validacion/js/validacionNIF.js
+++ U5/projecte5_Validacion/js/validacionNIF.js
@@ -2,7 +2,7 @@
  * Función para validar el nombre y apellidos del formulario
  */
 function validarNombre() {
-    let patron = /^[A-Za-záéíóúüàèiòÁÉÍÓÚÀÈÒÜñÑçÇ]{2,}$/;
+    let patron = /^[A-Za-záéíóúüàèiòÁÉÍÓÚÀÈÒÜñÑçÇ ]{2,}$/;
 
     this.className = "";
     if (patron.test(this.value)) {
@@ -13,9 +13,21 @@ function validarNombre() {
 /**
  * Función para validar el email del formulario
  */
+ function validarEmail() {
+    let patron = /^.+@.+$/;
+
+    this.className = "";
+    if (patron.test(this.value)) {
+        this.className = "verde";
+    }
+}
 
 window.addEventListener('load', function(){
     let nombre = document.getElementById('nombre');
+    let apellidos = document.getElementById('apellidos');
+    let email = this.document.getElementById('email');
 
     nombre.addEventListener('keyup', validarNombre);
+    apellidos.addEventListener('keyup', validarNombre);
+    email.addEventListener('keyup', validarEmail);
 });
