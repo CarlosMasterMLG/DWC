852af189f58f690a40b42f1e2b5996c92687e142
carlos_blanco <carlosblancofranco@gmail.com>
2023-01-27 20:44:20 +0100

    27 enero clase

--- U5/projecte5_Validacion/js/validacionNIF.js
+++ U5/projecte5_Validacion/js/validacionNIF.js
@@ -8,7 +8,14 @@ function validarDatos(){
     patrones.set("apellidos", /^[A-Za-záéíóúüàèiòÁÉÍÓÚÀÈÒÜñÑçÇ ]{2,}$/);
     patrones.set("email", /^.+@.+$/);
     patrones.set("telefonoNacional", /^([89][^09]|[67][0-9])[0-9]{7}$/);
-    //patrones.set("NIF", /^[0123456789]{8}.[A-Za-Z]{1}$/);
+    patrones.set("codigoPostal", /^(0[1-9]|[1-4][0-9]5[0-2])[0-9]{3}$/);
+    patrones.set("hora", /^([0-1][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/);
+    patrones.set("targetaVisa", /^4[0-9]{15}|4[0-9]{12}$/);
+    patrones.set("targetaMasterCard", /^5[1-5][0-9]{14}$/);
+    patrones.set("targetaDiscover", /^6011[0-9]{12}|5[0-9]{14}$/);
+    patrones.set("targetaAmericanExpress", /^34[0-9]{13}|37[0-9]{13}$/);
+    patrones.set("targetaDinersClub", /^36[0-9]{12}|38[0-9]{12}|30[0-5][0-9]{11}$/);
+    patrones.set("targetaJCB", /^35[0-9]{14}|2131[0-9]{11}|1800[0-9]{11}$/);
     
     this.className = "";
     if (patrones.get(this.id).test(this.value)) {
@@ -22,7 +29,6 @@ function validarDatos(){
     let nombre = document.getElementById('nombre');
     let apellidos = document.getElementById('apellidos');
     let email = document.getElementById('email');
-    let NIF = document.getElementById('NIF');
 
     nombre.addEventListener('keyup', validarDatos);
     apellidos.addEventListener('keyup', validarDatos);
