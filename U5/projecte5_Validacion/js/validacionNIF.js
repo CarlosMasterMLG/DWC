5dba173f21dab29041b870efe2adc564b11fa02b
carlos_blanco <carlosblancofranco@gmail.com>
2023-02-01 20:35:47 +0100

    1 febrero clase

--- U5/projecte5_Validacion/js/validacionNIF.js
+++ U5/projecte5_Validacion/js/validacionNIF.js
@@ -10,13 +10,25 @@ function validarDatos(){
     patrones.set("telefonoNacional", /^([89][^09]|[67][0-9])[0-9]{7}$/);
     patrones.set("codigoPostal", /^(0[1-9]|[1-4][0-9]5[0-2])[0-9]{3}$/);
     patrones.set("hora", /^([0-1][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/);
-    patrones.set("targetaVisa", /^4[0-9]{15}|4[0-9]{12}$/);
-    patrones.set("targetaMasterCard", /^5[1-5][0-9]{14}$/);
-    patrones.set("targetaDiscover", /^6011[0-9]{12}|5[0-9]{14}$/);
-    patrones.set("targetaAmericanExpress", /^34[0-9]{13}|37[0-9]{13}$/);
-    patrones.set("targetaDinersClub", /^36[0-9]{12}|38[0-9]{12}|30[0-5][0-9]{11}$/);
-    patrones.set("targetaJCB", /^35[0-9]{14}|2131[0-9]{11}|1800[0-9]{11}$/);
     
+    let targetaVisa ='4[0-9]{15}|4[0-9]{12}';
+    let targetaMasterCard = '5[1-5][0-9]{14}';
+    let targetaDiscover = '6011[0-9]{12}|5[0-9]{14}';
+    let targetaAmericanExpress = '34[0-9]{13}|37[0-9]{13}';
+    let targetaDinersClub = '36[0-9]{12}|38[0-9]{12}|30[0-5][0-9]{11}';
+    let targetaJCB = '35[0-9]{14}|2131[0-9]{11}|1800[0-9]{11}';
+
+    let tarjetas = `^(${targetaVisa}|${targetaMasterCard}|${targetaDiscover}|${targetaAmericanExpress}|${targetaDinersClub}|${targetaJCB})$`;
+
+    let patronTarjetas = new RegExp(`${tarjetas}`);
+    console.log(patronTarjetas);
+
+    patrones.set("tarjeta", `${patronTarjetas}`);
+    
+    //console.log(patrones);
+    
+
+
     this.className = "";
     if (patrones.get(this.id).test(this.value)) {
         this.className = "verde";
