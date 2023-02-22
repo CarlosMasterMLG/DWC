9cd12457985822693fb38f6c0a7fd97f6686aa90
carlos_blanco <carlosblancofranco@gmail.com>
2023-02-22 19:57:50 +0100

    22 febrero clase

--- recuDOM/202223_Ex_DOM_Eventos_2/Ej3/act3.js
+++ recuDOM/202223_Ex_DOM_Eventos_2/Ej3/act3.js
@@ -3,21 +3,11 @@ girarPalabra('carlos');
 function girarPalabra(palabra) {
     
     let arrayLetras = palabra.split('');
-    console.log(arrayLetras);
 
     let tamanyoArray = arrayLetras.length;
-    console.log(tamanyoArray);
 
-    let arrayOrdenado = [];
-    let j = arrayLetras.length;
+    arrayBien = arrayLetras.reverse();
 
-    for (let i = 0; i < arrayLetras.length; i++) {
-        
-        arrayOrdenado[i] = arrayLetras[j];
-        
-        j = j-1;
-    }
-
-    console.log(arrayLetras);
+    return arrayBien.toString();
 
 }
